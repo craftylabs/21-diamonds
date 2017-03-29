3255d2a7faddeaf5020febce0565e0490c23a1a8
sgtquezada <sgtquezada@gmail.com>
2017-03-29 15:32:18 -0700

    setting cookie for fb id in oauth callback

--- server/routes/auth/auth_controller.js
+++ server/routes/auth/auth_controller.js
@@ -1,6 +1,6 @@
 module.exports.loginFB = (req, res) => {
   console.log('made it to callback in facebook auth')
   res.cookie('accessToken', req.user.token, {expires: 0});
-  res.status(200).json(req.user);
-  res.redirect('http://localhost:8080');
+  res.cookie('facebookId', req.user.facebookId);
+  res.redirect('http://localhost:8080/app');
 }
